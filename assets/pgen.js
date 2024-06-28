5795bf753cd3dbb938b3d4cf1b7984dfe1256067
mathemolympiad <kaiserchan1023@gmail.com>
2024-06-28 14:34:05 +0800

    put database in pgen.js

--- assets/pgen.js
+++ assets/pgen.js
@@ -1,2 +1,23 @@
 import problems from "./problems.json" assert { type: 'json' };
 document.getElementById("problem").innerHTML = problems[Math.floor(Math.random() * 2)]["problem"];
+problems = [
+        {
+            "inputAns": false,
+            "diff": 1,
+            "area": "Algebra",
+            "topic": "Circles",
+            "source": "Original",
+            "problem": "Graph the equation \\((x-4)^2-16=-(y-1)^2.\\)",
+            "solution": ""
+        },
+        {
+            "inputAns": true,
+            "diff": 1,
+            "area": "Algebra",
+            "topic": "Circles",
+            "source": "Original",
+            "problem": "Write the equation of the circle that has diameter \\(\\overline{AB}\\) where \\(A(-2,3)\\) and \\(B(-5,7).\\) Express your answer in standard form.",
+            "answer": "",
+            "solution": ""
+        }
+    ]
