fbedf54afb1f0dfe82117e73c5a01d2f94f3a281
mathemolympiad <kaiserchan1023@gmail.com>
2024-06-28 14:24:28 +0800

    fix syntax

--- assets/pgen.js
+++ assets/pgen.js
@@ -1,2 +1,2 @@
-import problems from "./problems.json" assert { type: 'json' };;
+import problems from "./problems.json" assert { type: 'json' };
 document.getElementById("problem").innerHTML = problems[Math.floor(Math.random() * 2)].problem;
