21b4fcc83254123b1b159bb7da1d11725ef7f49b
mathemolympiad <kaiserchan1023@gmail.com>
2024-06-30 22:13:24 +0800

    add answer and solution display

--- assets/pgen.js
+++ assets/pgen.js
@@ -59,6 +59,21 @@ function giveProblem() {
     document.getElementById("topic").innerHTML = problems[index]["topic"];
     document.getElementById("diff").innerHTML = problems[index]["diff"];
     document.getElementById("id").innerHTML = index + 1;
+
+    if (problems[index]["answer"] == "") {
+        document.getElementById("answer").innerHTML = "The problem curator has not yet given an answer for this problem.";
+    }
+    else {
+        document.getElementById("answer").innerHTML = problems[index]["answer"];
+    }
+
+    if (problems[index]["solution"] == "") {
+        document.getElementById("solution").innerHTML = "The problem curator has not yet given a solution for this problem.";
+    }
+    else {
+        document.getElementById("solution").innerHTML = problems[index]["solution"];
+    }
+
     MathJax.typeset();
 }
 
